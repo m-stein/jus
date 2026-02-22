048d69e2544e2ffc1b8a8fe95e83e18bac07e8c3
Martin Stein <martin.stein.2023@gmail.com>
2026-02-22 20:57:17 +0100

    Provide public interface via jus.d.ts file

---
+++ jus.d.ts
@@ -0,0 +1,27 @@
+/**
+ * This file originates from the Jus game engine.
+ *
+ * If you want to use Jus in your JavaScript project, the file can provide the
+ * public interface of Jus to your type server, thereby enabling type checking,
+ * import checking, and code completion. Just copy the correct version of this
+ * file to the types/ sub-directory in your project directory and make sure to
+ * add it to the "include" array in your jsconfig.json file.
+ */
+declare module 'jus' {
+    export type BlueprintParams = Record<
+        string,
+        string | number | boolean | null
+    >;
+    export type Blueprint = {
+        constructor: new (args: any) => any;
+        context?: Record<string, any>;
+    };
+    export class ObjectFactory {
+        constructor(blueprintsArray: Blueprint[]);
+        createObj(blueprintName: string, params?: BlueprintParams): any;
+        createObjFromJson(json: {
+            class: string;
+            params?: Record<string, any>;
+        }): any;
+    }
+}
