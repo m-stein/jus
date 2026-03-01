2b3fcc515c384b1213fe51ac76ab96ed2019b72e
Martin Stein <martin.stein.2023@gmail.com>
2026-03-01 11:18:42 +0100

    Try automating dts-file creation

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 Jus is a vanilla JavaScript game engine for 2D games that renders directly
 to an HTML canvas. It aims for providing versatile tools for game development
 while imposing as few framework as possible.
+
+## How to use the engine in your project
+
+If you want to use Jus in your JavaScript project, the file types/jus.d.ts
+can provide the public interface of Jus to your type server, thereby enabling
+type checking, import checking, and code completion. Just copy the correct
+version of this file to the types/ sub-directory in your project directory and
+make sure to add it to the "include" array in your jsconfig.json file.
+
+## How to update the type declarations file
+
+! > npm install -g typescript
+! > tsc
---
+++ types/jus.d.ts
@@ -0,0 +1,116 @@
+declare module "object_factory" {
+    /**
+     * @typedef {{
+     *   constructor: new (args: any) => any,
+     *   context?: Record<string, any>
+     * }} Blueprint
+     * @typedef {Record<string, Blueprint>} BlueprintDict
+     */
+    export class ObjectFactory {
+        /**
+         * @param {Blueprint[]} blueprintsArray
+         */
+        constructor(blueprintsArray: Blueprint[]);
+        /** @type {Record<string, Blueprint>} */
+        _blueprints: Record<string, Blueprint>;
+        /**
+         * @param {string} blueprintName
+         * @param {Record<string, string | number | boolean | null>} params
+         * @returns {InstanceType<BlueprintDict[string]['constructor']>}
+         */
+        createObj(blueprintName: string, params?: Record<string, string | number | boolean | null>): InstanceType<BlueprintDict[string]["constructor"]>;
+        /**
+         * @param {{ class: string, params?: Record<string, any> }} json
+         * @returns {InstanceType<BlueprintDict[string]['constructor']>}
+         */
+        createObjFromJson(json: {
+            class: string;
+            params?: Record<string, any>;
+        }): InstanceType<BlueprintDict[string]["constructor"]>;
+    }
+    export type Blueprint = {
+        constructor: new (args: any) => any;
+        context?: Record<string, any>;
+    };
+    export type BlueprintDict = Record<string, Blueprint>;
+}
+declare module "json_file" {
+    export class JsonFile {
+        /**
+         * @param {Document} htmlDocument
+         * @param {JSON} jsonParser
+         * @param {string} relPath
+         * @param {(file: JsonFile) => void} onLoaded
+         */
+        constructor(htmlDocument: Document, jsonParser: JSON, relPath: string, onLoaded: (file: JsonFile) => void);
+        _relPath: string;
+        _onLoaded: (file: JsonFile) => void;
+        _jsonParser: JSON;
+        _httpRequest: XMLHttpRequest;
+        _url: URL;
+        /**
+         * @type {() => void}
+         * @private
+         */
+        private _onReadyStateChange;
+        _data: any;
+        get data(): any;
+    }
+}
+declare module "vector_2" {
+    export class Vector2 {
+        /**
+         * @param {number} x
+         * @param {number} y
+         */
+        constructor(x: number, y: number);
+        x: number;
+        y: number;
+        /**
+         * @param {Vector2} other
+         * @returns {number}
+         */
+        distanceTo(other: Vector2): number;
+        /**
+         * @returns {Vector2}
+         */
+        copy(): Vector2;
+        /**
+         * @param {Vector2} other
+         * @returns {boolean}
+         */
+        equals(other: Vector2): boolean;
+        /**
+         * @param {Vector2} other
+         * @returns {Vector2}
+         */
+        add(other: Vector2): Vector2;
+        /**
+         * @param {number} factor
+         * @returns {Vector2}
+         */
+        scale(factor: number): Vector2;
+        /**
+         * @param {Vector2} other
+         * @returns {Vector2}
+         */
+        subtract(other: Vector2): Vector2;
+        /**
+         * @returns {number}
+         */
+        length(): number;
+        /**
+         * @returns {Vector2}
+         */
+        normalized(): Vector2;
+        /**
+         * @returns {string}
+         */
+        toString(): string;
+    }
+}
+declare module "index" {
+    export { ObjectFactory } from "./object_factory.js";
+    export { JsonFile } from "./json_file.js";
+    export { Vector2 } from "./vector_2.js";
+}
